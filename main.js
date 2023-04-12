b77805efa953bab2a4ad369272e51ba0762254f8
Victor Izquierdo <vhmizq@gmail.com>
2023-04-11 21:26:07 -0600

    new js function to toggle classes

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const accordion = document.getElementsByClassName('container');
+const labels = document.getElementsByClassName('label');
 
-for (i = 0; i < accordion.length; i++) {
-  accordion[i].addEventListener('click', function () {
-    this.classList.toggle('active');
-  });
+const changeClass = (elements, classItem) => {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', function () {
+      this.classList.toggle(classItem);
+    });
+  }
 }
+
+changeClass(accordion, 'active');
+changeClass(labels, 'active');
