abf48dfcff43cbbb0cedb611214d7d1bfeae4f6f
Victor Izquierdo <vhmizq@gmail.com>
2023-04-11 21:07:42 -0600

    need fix img container in Y

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const accordion = document.getElementsByClassName'container');
+const accordion = document.getElementsByClassName('container');
 
 for (i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener('click', function () {
